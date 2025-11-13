4e7e5d4cbc1a220f4829f444dfff0f206a4c57e2
AdrielIoasys <adrielbento@ioasys.com.br>
2025-11-13 06:53:50 -0300

    feat: add deleted_at column to decks table for soft deletion support

--- priv/repo/migrations/20251109143729_add_decks_table.exs
+++ priv/repo/migrations/20251109143729_add_decks_table.exs
@@ -6,6 +6,7 @@ defmodule EngramAPI.Repo.Migrations.AddDecksTable do
       add :name, :string, size: 120
       add :description, :string, size: 120
       add :icon, :string, size: 120
+      add :deleted_at, :naive_datetime, null: true, default: nil
       add :collection_id, references(:collections, type: :binary_id, on_delete: :nothing)
 
       timestamps()
